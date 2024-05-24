433a85552606f36e98cb55912a7c009ca188742c
sampath pasupalati <sampath.p@skil.ai>
2024-05-24 15:53:36 +0530

    mongo db function changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,19 +3,35 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const uri = process.env.MONGO_URI;
 let db;
 
 const connectToDatabase = async () => {
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    await client.connect();
-    db = client.db();
-    console.log('MongoDB connected');
+    if (db) {
+        return db;
+    }
+
+    const client = new MongoClient(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        tls: true,  // Enable TLS/SSL
+        tlsAllowInvalidCertificates: false,  // Adjust if your server uses self-signed certificates
+        tlsAllowInvalidHostnames: false  // Adjust if your server uses self-signed certificates
+    });
+
+    try {
+        await client.connect();
+        db = client.db();
+        console.log('Connected to MongoDB');
+        return db;
+    } catch (err) {
+        console.error('Failed to connect to the database', err);
+        throw err;
+    }
 };
 
 const getDb = () => {
     if (!db) {
-        throw new Error('Database not connected');
+        throw new Error('Database not initialized. Call connectToDatabase first.');
     }
     return db;
 };
